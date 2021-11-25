12d3544d3b7228e22f51901b9609895f28e71644
Renan Loureiro <renanpl47@gmail.com>
2021-11-24 22:38:15 -0300

    organizando o codigo

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -111,14 +111,13 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
         (produto) => produto.id === productId
       )
 
-      if (!productExists) {
+      if (productExists) {
+        productExists.amount = amount
+        setCart([...updatedCart])
+        localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart))
+      } else {
         throw new Error()
       }
-
-      productExists.amount = amount
-
-      setCart([...updatedCart])
-      localStorage.setItem("@RocketShoes:cart", JSON.stringify(updatedCart))
     } catch {
       toast.error("Erro na alteração de quantidade do produto")
     }
